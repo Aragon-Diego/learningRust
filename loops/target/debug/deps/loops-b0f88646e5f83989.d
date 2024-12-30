5a95cbe352babfb66ca2523401b3eefff32ae184
aragon-diego <diego.aragoncursos@gmail.com>
2024-12-30 00:54:41 -0600

    chapter 3 Done!

---
+++ loops/target/debug/deps/loops-b0f88646e5f83989.d
@@ -0,0 +1,5 @@
+/Users/starman/Documents/code/the-rust-book/loops/target/debug/deps/loops-b0f88646e5f83989: src/main.rs
+
+/Users/starman/Documents/code/the-rust-book/loops/target/debug/deps/loops-b0f88646e5f83989.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/Users/starman/Documents/code/the-rust-book/loops/target/debug/loops: /Users/starman/Documents/code/the-rust-book/loops/src/main.rs
